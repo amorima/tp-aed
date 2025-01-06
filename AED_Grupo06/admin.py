6dc40a9244c209b173d3e5d7738a78e8854fdd5b
Gabriel-S-Paiva <mr.sousapaiva@gmail.com>
2025-01-06 15:37:49 +0000

    Admin Functions Updated

--- AED_Grupo06/admin.py
+++ AED_Grupo06/admin.py
@@ -74,3 +74,130 @@ def addAdmin(user):
     for line in newUserList:
         file.write(line)
     file.close()
+
+def addCat(categoryName):
+    """
+    Arg-Category Name|str
+    Returns-Add the Category name to the categorias.txt file
+    """
+    categoryList=lerFicheiro(category_db)
+    newCategoryList=[]
+    for category in categoryList:
+        campos=category.split(";")
+        main=campos[0][0:2]
+    try:
+        int(main)
+    except:
+        main="00"
+        newCategoryList.append(main+";"+categoryName+"\n")
+    else:
+        if len(str(int(main)))==2 and main!="09":
+            newCategoryList.append(str(int(main)+1)+";"+categoryName+"\n")
+        elif main=="09":
+            newCategoryList.append(str(int(main)+1)+";"+categoryName+"\n")
+        else:
+            newCategoryList.append("0"+str(int(main)+1)+";"+categoryName+"\n")
+    file= open(category_db,"a",encoding="utf-8")
+    for line in newCategoryList:
+        file.write(line)
+    file.close()
+
+def removeCat(categoryName):
+    """
+    Arg-Category Name|str
+    Returns-Removes the Category (as well as all the sub-categories associated) from the categorias.txt
+    """
+    categoryList=lerFicheiro(category_db)
+    newCategoryList=["Main(XX).Sub(.XX);Name\n"]
+    categoryToDelete="N/a"
+    for line in categoryList:
+        campos=line.split(";")
+        if campos[1]==categoryName+"\n":
+            categoryToDelete=campos[0]
+        try:
+            if campos[0][0:2]!=categoryToDelete and int(campos[0][0:2])<int(categoryToDelete):
+                newCategoryList.append(line)
+            elif campos[0][0:2]!=categoryToDelete:
+                if len(str(int(campos[0][0:2])))==1 or campos[0][0:2]=="10":
+                    try:
+                        sub=campos[0][2:5]
+                    except:
+                        sub=""
+                    newCategoryList.append("0"+str(int(campos[0][0:2])-1)+sub+";"+campos[1])
+                else:
+                    try:
+                        sub=campos[0][2:5]
+                    except:
+                        sub=""
+                    newCategoryList.append(str(int(campos[0][0:2])-1)+sub+";"+campos[1])
+        except:continue
+    file= open(category_db,"w",encoding="utf-8")
+    for line in newCategoryList:
+        file.write(line)
+    file.close()
+    
+def addSubCat(mainCategory,subCategory):
+    """
+    Arg-mainCategory|str, subCategory|str
+    Returns-Adds the sub-category to the main category given in the categorias.txt file
+    """
+    categoryList=lerFicheiro(category_db)
+    newCategoryList=[]
+    sub=".00"
+    main="N/a"
+    for categoryLine in categoryList:
+        campos=categoryLine.split(";")
+        if campos[1]==mainCategory+"\n":
+            main=campos[0]
+            mainAdd=categoryLine
+        if campos[0][0:3]==main+".":
+            lastSub=campos[0][3:5]
+            fullLine=categoryLine
+            if len(str(int(lastSub)))==2:
+                sub="."+str(int(lastSub)+1)
+            elif lastSub=="09":
+                sub=".10"
+            else:
+                sub=".0"+str(int(lastSub)+1)
+        newCategoryList.append(categoryLine)#index
+    try:
+        indexAdd=newCategoryList.index(fullLine)+1
+    except:
+        indexAdd=newCategoryList.index(mainAdd)+1
+    newCategoryList.insert(indexAdd,main+sub+";"+subCategory+"\n")
+    file= open(category_db,"w",encoding="utf-8")
+    for line in newCategoryList:
+        file.write(line)
+    file.close()
+
+def removeSubCat(mainCategory,subCategory):
+    """
+    Arg-mainCategory|str, subCategory|str
+    Returns-Removes the sub-category to the main category given in the categorias.txt file
+    """
+    categoryList=lerFicheiro(category_db)
+    newCategoryList=[]
+    mainCode=subCode="100"
+    for line in categoryList:
+        campos=line.split(";")
+        codes=campos[0].split(".")
+        if campos[1]==mainCategory+"\n":
+            mainCode=codes[0]
+        if campos[1]==subCategory+"\n":
+            subCode=codes[1]
+        if codes[0]!=mainCode:
+            newCategoryList.append(line)
+        elif campos[0][0:3]==codes[0]+".":
+            if int(codes[1])<int(subCode):
+                newCategoryList.append(line)
+            elif int(codes[1])>int(subCode):
+                if len(str(int(codes[1])))==2  and codes[1]!="10":
+                    newCategoryList.append(codes[0]+"."+str(int(codes[1])-1)+";"+campos[1])
+                else:
+                    newCategoryList.append(codes[0]+".0"+str(int(codes[1])-1)+";"+campos[1])
+        else:
+            newCategoryList.append(line)
+    file= open(category_db,"w",encoding="utf-8")
+    for line in newCategoryList:
+        file.write(line)
+    file.close()
