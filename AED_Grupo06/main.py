a422a59054905586008f7edb9e98490200328dce
Gabriel-S-Paiva <mr.sousapaiva@gmail.com>
2025-01-07 13:25:14 +0000

    Conecting to main.py

--- AED_Grupo06/main.py
+++ AED_Grupo06/main.py
@@ -3,11 +3,13 @@
 #-----------
 import os
 import time
+from gui import *
 
 
 def splash():
     """
     Apresenta uma imagem temporaria
     """
+    openLoad()
     time.sleep(3)#waiting time
     #jump to login
--- AED_Grupo06/tkinter/gui.py
+++ AED_Grupo06/tkinter/gui.py
@@ -14,30 +14,30 @@ ASSETS_PATH = OUTPUT_PATH / Path(r".\assets\frame0")
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
+def openLoad():
+    window = Tk()
 
-window = Tk()
-
-window.geometry("1200x675")
-window.configure(bg = "#121212")
+    window.geometry("1200x675")
+    window.configure(bg = "#121212")
 
 
-canvas = Canvas(
-    window,
-    bg = "#121212",
-    height = 675,
-    width = 1200,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
+    canvas = Canvas(
+        window,
+        bg = "#121212",
+        height = 675,
+        width = 1200,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
+    )
 
-canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    600.0,
-    338.0,
-    image=image_image_1
-)
-window.resizable(False, False)
-window.mainloop()
+    canvas.place(x = 0, y = 0)
+    image_image_1 = PhotoImage(
+        file=relative_to_assets("image_1.png"))
+    image_1 = canvas.create_image(
+        600.0,
+        338.0,
+        image=image_image_1
+    )
+    window.resizable(False, False)
+    window.mainloop()
